34be14e3b144b8c0034af771333765ab009c5ecc
JosueRhea <josueroyale8171@gmail.com>
2023-09-27 15:12:38 -0700

    Add some tweaks

--- apps/web/components/select-site.tsx
+++ apps/web/components/select-site.tsx
@@ -1,11 +1,14 @@
 "use client";
 import { Site } from "@openanalytics/db/src/schema";
+import { PlusIcon } from "@radix-ui/react-icons";
 import { Select } from "@radix-ui/themes";
 import { useParams, useRouter } from "next/navigation";
 
 interface Props {
   data: Site[];
 }
+
+const ADD_SITE_KEY = "Add site"
 
 export const SelectSite = ({ data }: Props) => {
   const params = useParams();
@@ -14,15 +17,16 @@ export const SelectSite = ({ data }: Props) => {
   const currentSiteId = params?.siteId as string;
 
   if (!currentSiteId) return null;
+
   return (
     <Select.Root
       onValueChange={(value) => {
         router.push(`/dashboard/${value}`);
       }}
-      value={currentSiteId}
+      value={currentSiteId ?? ADD_SITE_KEY}
     >
       <Select.Trigger variant="soft" placeholder="Select a site..." />
-      <Select.Content>
+      <Select.Content highContrast>
         <Select.Group>
           <Select.Label>Sites</Select.Label>
           {data.map((site) => (
@@ -30,6 +34,9 @@ export const SelectSite = ({ data }: Props) => {
               {site.name}
             </Select.Item>
           ))}
+          <Select.Item  value={ADD_SITE_KEY}>
+            <PlusIcon width={10} height={10} /> {ADD_SITE_KEY}
+          </Select.Item>
         </Select.Group>
       </Select.Content>
     </Select.Root>
